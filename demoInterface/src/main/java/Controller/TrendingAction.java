87dd2922e2465303c4953884f178b1cfbfb58283
job <36887301+supergreditur@users.noreply.github.com>
2018-06-09 21:43:54 +0200

    it works!

--- demoInterface/src/main/java/Controller/TrendingAction.java
+++ demoInterface/src/main/java/Controller/TrendingAction.java
@@ -5,15 +5,34 @@ import Frame.MainWindow;
 
 import javax.swing.*;
 import java.awt.event.ActionEvent;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 import java.util.List;
 
 public class TrendingAction extends AbstractAction {
-
+    private List<CompanyTweet> comp;
+    private MainWindow window;
     public TrendingAction(String s, List<CompanyTweet> comp, MainWindow window){
         super(s);
+        this.comp = comp;
+        this.window =window;
     }
     @Override
     public void actionPerformed(ActionEvent e) {
+        comp.sort((c1, c2) -> {
+            if(trend(c1) == trend(c2)){
+                return 0;
+            }
+            return trend(c1) > trend(c2) ? -1 : 1;
+        });
+        window.updateButtons(comp);
+    }
 
+    private double trend(CompanyTweet x){
+        Calendar  now = GregorianCalendar.getInstance();
+        Calendar calendar = GregorianCalendar.getInstance();
+        calendar.setTime(x.getStatus().getCreatedAt());
+
+        return (x.getStatus().getFavoriteCount()+x.getStatus().getRetweetCount())/(double)(now.getTimeInMillis()-calendar.getTimeInMillis());
     }
 }
--- demoInterface/src/main/java/Frame/ToolBar.java
+++ demoInterface/src/main/java/Frame/ToolBar.java
@@ -9,20 +9,17 @@ public class ToolBar extends JToolBar {
     public ToolBar (List<CompanyTweet> comp, MainWindow window) {
         JButton likes = new JButton();
         JButton retweets = new JButton();
-        JButton followers = new JButton();
         JButton trending = new JButton();
         JButton recent = new JButton();
 
         likes.setAction(new LikeAction("Most Likes",comp,window));
         retweets.setAction(new RetweetAction("Most Retweets",comp,window));
-        followers.setAction(new FollowerAction("Most Followers",comp,window));
         trending.setAction(new TrendingAction("Trending",comp,window));
         recent.setAction(new RecentAction("Newest",comp,window));
 
         this.setRollover(true);
         this.add(recent);
         this.addSeparator();
-        this.add(followers);
         this.addSeparator();
         this.add(likes);
         this.addSeparator();
