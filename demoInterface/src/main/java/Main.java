c88ce49910d78b386a83c311e2c0009b77c030fb
ghidirimschi <nghidirimschi@gmail.com>
2018-06-09 12:57:07 +0200

    -geez Sander

--- demoInterface/src/main/java/Main.java
+++ demoInterface/src/main/java/Main.java
@@ -1,16 +1,26 @@
+import com.teamdev.jxbrowser.chromium.Browser;
+import com.teamdev.jxbrowser.chromium.swing.BrowserView;
+
 import javax.swing.*;
 import java.awt.*;
 
+/**
+ * This sample demonstrates how to create Browser instance,
+ * embed it into Swing BrowserView container, display it in JFrame and
+ * navigate to the "www.google.com" web site.
+ */
 public class Main {
-    public static void main(String args[]) throws Exception{
-        JEditorPane website = new JEditorPane("https://www.google.nl/");
-        website.setEditable(false);
+    public static void main(String[] args) {
+        Browser browser = new Browser();
+        BrowserView browserView = new BrowserView(browser);
 
-        JFrame frame = new JFrame("Google");
-        frame.add(new JScrollPane(website));
-        frame.setSize(new Dimension(500,500));
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        JFrame frame = new JFrame();
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.add(browserView, BorderLayout.CENTER);
+        frame.setSize(700, 500);
+        frame.setLocationRelativeTo(null);
         frame.setVisible(true);
-        frame.pack();
+
+        browser.loadURL("http://www.google.com");
     }
 }
