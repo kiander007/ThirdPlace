511b75705a4252f7d4191cff797af50a2abdbc75
job <36887301+supergreditur@users.noreply.github.com>
2018-06-09 21:12:03 +0200

    fixed

--- demoInterface/src/main/java/Controller/RecentAction.java
+++ demoInterface/src/main/java/Controller/RecentAction.java
@@ -8,12 +8,21 @@ import java.awt.event.ActionEvent;
 import java.util.List;
 
 public class RecentAction extends AbstractAction {
-
+    private List<CompanyTweet> comp;
+    private MainWindow window;
     public RecentAction(String s, List<CompanyTweet> comp, MainWindow window){
         super(s);
+        this.comp = comp;
+        this.window = window;
     }
     @Override
     public void actionPerformed(ActionEvent e) {
-
+        comp.sort((c1, c2) -> {
+            if(c1.getStatus().getCreatedAt().equals(c2.getStatus().getRetweetCount())){
+                return 0;
+            }
+            return c1.getStatus().getRetweetCount() > c2.getStatus().getRetweetCount() ? -1 : 1;
+        });
+        window.updateButtons(comp);
     }
 }
